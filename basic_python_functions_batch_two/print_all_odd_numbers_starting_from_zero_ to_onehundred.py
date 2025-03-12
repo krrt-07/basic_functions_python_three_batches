c5287fa07bfa4b82748a5e3e0681adb7811797aa
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 23:46:29 +0800

    make a condition to check if the number is odd.

--- basic_python_functions_batch_two/print_all_odd_numbers_starting_from_zero_ to_onehundred.py
+++ basic_python_functions_batch_two/print_all_odd_numbers_starting_from_zero_ to_onehundred.py
@@ -5,5 +5,6 @@ while count <100:
     count += 1
     number = int(input("Enter a number: "))
     # make a condition to check if the number is odd.
+    if number % 2 != 0:
     
     # print the odd numbers.
