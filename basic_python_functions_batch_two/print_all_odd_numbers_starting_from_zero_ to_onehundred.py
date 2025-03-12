b71e7a1dc784ffda2cd3967f3cd4e61705ed3fd9
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 23:44:38 +0800

    use while loop to run 100 times.

--- basic_python_functions_batch_two/print_all_odd_numbers_starting_from_zero_ to_onehundred.py
+++ basic_python_functions_batch_two/print_all_odd_numbers_starting_from_zero_ to_onehundred.py
@@ -1,5 +1,7 @@
 # set count to 0.
 count = 0
 # use while loop to run 100 times.
+while count <100:
+    count += 1
     # make a condition to check if the number is odd.
     # print the odd numbers.
