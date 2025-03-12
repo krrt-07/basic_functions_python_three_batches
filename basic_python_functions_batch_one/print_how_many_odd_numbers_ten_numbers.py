982372dcf8e27df9c369945acf5e190d3f460875
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 18:13:48 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_one/print_how_many_odd_numbers_ten_numbers.py
@@ -0,0 +1,4 @@
+# set count and sum to 0
+# make a while loop that will run ten times to ask the user for a number.
+# make a condition to check if the number is odd.
+# make a command that will count how many odd numbers is there and print it.
