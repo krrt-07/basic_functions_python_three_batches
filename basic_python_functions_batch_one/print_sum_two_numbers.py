27e40a0f1400b58c65866f76145124155a77c583
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 14:40:02 +0800

    get the second number.

--- basic_python_functions_batch_one/print_sum_two_numbers.py
+++ basic_python_functions_batch_one/print_sum_two_numbers.py
@@ -1,4 +1,7 @@
 # ask for the first number.
 first_number = int(input("Enter the first number: "))
+
 # ask for the second number.
+second_number = int(input("Enter the second number: "))
+
 # print the sum of the two numbers.
