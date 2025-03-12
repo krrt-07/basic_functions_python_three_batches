34021ccc0c6e13006fe2bdccf8dfe76b8a593461
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 18:06:47 +0800

    make a while loop that will run ten times to ask the user for a number.

--- basic_python_functions_batch_one/print_sum_ten_numbers.py
+++ basic_python_functions_batch_one/print_sum_ten_numbers.py
@@ -2,5 +2,9 @@
 count = 0
 sum = 0
 # make a while loop that will run ten times to ask the user for a number.
+while count < 10:
+    # ask for a number.
+    number = int(input("Enter a number:"))
+    count += 1
 # make a command that add all numbers.
 # print the sum of all numbers.
