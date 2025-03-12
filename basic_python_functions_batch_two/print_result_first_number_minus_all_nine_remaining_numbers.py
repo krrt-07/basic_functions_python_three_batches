a4bbc0d078954653cbae08318229e42ae381a84c
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 23:16:42 +0800

    ask for the first number.

--- basic_python_functions_batch_two/print_result_first_number_minus_all_nine_remaining_numbers.py
+++ basic_python_functions_batch_two/print_result_first_number_minus_all_nine_remaining_numbers.py
@@ -1,5 +1,6 @@
 # set count to 0.
 count = 0
 # ask for the first number.
+difference = int(input('Enter a number: '))
 # use while loop to run nine times to ask for a number.
 # print the difference.
