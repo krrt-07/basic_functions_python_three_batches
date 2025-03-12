912d4a6e206bedf136fb165dad40440f969dfc5c
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 18:14:23 +0800

    set count and sum to 0.

--- basic_python_functions_batch_one/print_how_many_odd_numbers_ten_numbers.py
+++ basic_python_functions_batch_one/print_how_many_odd_numbers_ten_numbers.py
@@ -1,4 +1,6 @@
-# set count and sum to 0
+# set count and sum to 0.
+count = 0
+sum = 0
 # make a while loop that will run ten times to ask the user for a number.
 # make a condition to check if the number is odd.
 # make a command that will count how many odd numbers is there and print it.
