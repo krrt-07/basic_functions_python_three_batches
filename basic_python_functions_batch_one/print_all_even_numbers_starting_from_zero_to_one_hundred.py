6766206edf8229994fc53ea0ef051f8cfffa3382
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 18:38:39 +0800

    add step to disregard odd numbers.

--- basic_python_functions_batch_one/print_all_even_numbers_starting_from_zero_to_one_hundred.py
+++ basic_python_functions_batch_one/print_all_even_numbers_starting_from_zero_to_one_hundred.py
@@ -1,4 +1,4 @@
 # use for-loop to repeatedly print numbers from 0 to 100
-for even_numbers in range(0, 101)
 # add step to disregard odd numbers.
+for even_numbers in range(0, 101, 2):
 # print all even numbers from 0 to 100.
