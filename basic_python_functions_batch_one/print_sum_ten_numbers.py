63eb1d56c138b7decc54d5c7832648d7edfbf972
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 18:04:19 +0800

    setting count and sum to 0.

--- basic_python_functions_batch_one/print_sum_ten_numbers.py
+++ basic_python_functions_batch_one/print_sum_ten_numbers.py
@@ -1,5 +1,6 @@
-# set count to 0
-# set sum to 0
+# set count to 0 and set sum to 0
+count = 0
+sum = 0
 # make a while loop that will run ten times to ask the user for a number.
 # make a command that add all numbers.
 # print the sum of all numbers.
