5f189994c0bd349752a4b080d40c8b55713d39f8
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 23:45:51 +0800

    use while loop to run 100 times.

--- basic_python_functions_batch_two/print_all_odd_numbers_starting_from_zero_ to_onehundred.py
+++ basic_python_functions_batch_two/print_all_odd_numbers_starting_from_zero_ to_onehundred.py
@@ -3,5 +3,7 @@ count = 0
 # use while loop to run 100 times.
 while count <100:
     count += 1
+    number = int(input("Enter a number: "))
     # make a condition to check if the number is odd.
+    
     # print the odd numbers.
