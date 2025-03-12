25c459d272791643098ff9e95f3ef2b8027f094d
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 18:48:19 +0800

    make a condition to disregard numbers ending in zero.

--- basic_python_functions_batch_one/print_all_numbers_starting_from_0_to_100_except_numbers_ending_zero.py
+++ basic_python_functions_batch_one/print_all_numbers_starting_from_0_to_100_except_numbers_ending_zero.py
@@ -1,4 +1,5 @@
 # use for-loop to print numbers from 0 to 100.
 for number in range(101):
 # make a condition to disregard numbers ending in zero.
+    if number % 10 != 0:
 # print all numbers from 0 to 100 except numbers ending in zero.
