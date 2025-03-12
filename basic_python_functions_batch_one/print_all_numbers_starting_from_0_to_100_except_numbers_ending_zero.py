d04f66f3bded2d4a8cb049970b4b6a17265365e4
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 18:47:18 +0800

    use for-loop to print numbers from 0 to 100.

--- basic_python_functions_batch_one/print_all_numbers_starting_from_0_to_100_except_numbers_ending_zero.py
+++ basic_python_functions_batch_one/print_all_numbers_starting_from_0_to_100_except_numbers_ending_zero.py
@@ -1,3 +1,4 @@
 # use for-loop to print numbers from 0 to 100.
+for number in range(101):
 # make a condition to disregard numbers ending in zero.
 # print all numbers from 0 to 100 except numbers ending in zero.
