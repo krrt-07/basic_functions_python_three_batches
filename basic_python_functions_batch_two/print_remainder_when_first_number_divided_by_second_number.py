97db87d1d41d8d8c80e22e708483109d63c2af0d
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 23:03:41 +0800

    ask for the second number.

--- basic_python_functions_batch_two/print_remainder_when_first_number_divided_by_second_number.py
+++ basic_python_functions_batch_two/print_remainder_when_first_number_divided_by_second_number.py
@@ -1,5 +1,6 @@
 # ask for the first number.
 input("Enter the first number: ")
 # ask for the second number.
+input("Enter the second number: ")
 # make a calculation to get the remainder when the first number is divideed by the second number.
 # print the remainder.
