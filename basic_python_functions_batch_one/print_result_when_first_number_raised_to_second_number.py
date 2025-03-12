9b74e4ecef92e496cac8c5163f55482da58a6518
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 17:54:31 +0800

    print the result of raising a the first number to second number.

--- basic_python_functions_batch_one/print_result_when_first_number_raised_to_second_number.py
+++ basic_python_functions_batch_one/print_result_when_first_number_raised_to_second_number.py
@@ -1,7 +1,8 @@
 # ask for the first number.
-input("Enter the first number: ")
+first_number = int(input("Enter the first number: "))
 
 # ask for the second number.
-input("Enter the second number: ")
+second_number = int(input("Enter the second number: "))
 
 # print the result of raising the first number to the power of the second number.
+print(first_number ** second_number)
