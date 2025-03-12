480c06479e819032e7246b7c8dd3daf72f527ad7
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 13:35:34 +0800

    input for second number

--- basic_python_functions_batch_one/get_bigger_number_between_two_numbers.py
+++ basic_python_functions_batch_one/get_bigger_number_between_two_numbers.py
@@ -1,5 +1,6 @@
 # ask for the first number.
 input("Enter the first number: ")
 # ask for the second number.
+input("Enter the second number: ")
 # make a decision on which number is bigger.
 # print the bigger number.
