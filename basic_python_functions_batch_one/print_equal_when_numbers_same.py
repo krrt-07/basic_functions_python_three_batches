59219ffaf00b292065dcdb7123690a63c17dc2cb
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 14:26:08 +0800

    get the second number

--- basic_python_functions_batch_one/print_equal_when_numbers_same.py
+++ basic_python_functions_batch_one/print_equal_when_numbers_same.py
@@ -1,5 +1,8 @@
 # ask for a number.
 input("Enter the first number: ")
+
 # ask again for another number.
+input("Enter the second number: ")
+
 # make a decision if number are the same.
 # print "Equal" if numbers are the same.
