03f1628ac9de2e3aa3fcda3eba37a6a05a5daa8f
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 13:42:42 +0800

    print the bigger number

--- basic_python_functions_batch_one/get_bigger_number_between_two_numbers.py
+++ basic_python_functions_batch_one/get_bigger_number_between_two_numbers.py
@@ -3,5 +3,5 @@ first_number = int(input("Enter the first number: "))
 # ask for the second number.
 second_number = int(input("Enter the second number: "))
 # make a decision on which number is bigger.
-max(first_number, second_number)
 # print the bigger number.
+print(max(first_number, second_number))
