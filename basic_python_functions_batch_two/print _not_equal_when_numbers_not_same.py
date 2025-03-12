9ae1633d99b9296fbaa5c6e228f8504efb1072c1
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 22:26:24 +0800

    ask for the second number.

--- basic_python_functions_batch_two/print _not_equal_when_numbers_not_same.py
+++ basic_python_functions_batch_two/print _not_equal_when_numbers_not_same.py
@@ -1,4 +1,5 @@
 # ask for the first number.
 input("Enter the first number: ")
 # ask for the second number.
+input("Enter the second number: ")
 # print "Not Equal" if the two numbers are not the same.
