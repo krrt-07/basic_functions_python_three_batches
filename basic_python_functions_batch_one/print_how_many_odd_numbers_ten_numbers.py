ee0dab087cae20e79f7d8165925b68c95f9641cd
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 18:16:01 +0800

    make a while loop that will run ten times to ask the user for a number.

--- basic_python_functions_batch_one/print_how_many_odd_numbers_ten_numbers.py
+++ basic_python_functions_batch_one/print_how_many_odd_numbers_ten_numbers.py
@@ -2,5 +2,9 @@
 count = 0
 sum = 0
 # make a while loop that will run ten times to ask the user for a number.
+while count < 10:
+    # asd for a number.
+    number = int(input("Enter a number: "))
+    count += 1
 # make a condition to check if the number is odd.
 # make a command that will count how many odd numbers is there and print it.
