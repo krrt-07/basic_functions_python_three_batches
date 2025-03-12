e47d090e40ae971d8a734f1fde6a5aa763b26701
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 23:32:56 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_two/in_ten_numbers_print_how_many_even_numbers.py
@@ -0,0 +1,4 @@
+# set count to 0 and set sum to 0.
+# make a while loop that will run ten times to ask for a number.
+    # make a condition to check if the number is even and list it.
+# print the counted even numbers.
