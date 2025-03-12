89304ce87976289ad470d22fbf889f6117e22b02
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 22:22:03 +0800

    print the smaller number.

--- basic_python_functions_batch_two/get_the_smaller_number.py
+++ basic_python_functions_batch_two/get_the_smaller_number.py
@@ -1,5 +1,7 @@
 # ask for the first number.
-input("Enter the first number: ")
+first_number = float(input("Enter the first number: "))
 # ask for the second number.
-input("Enter the second number: ")
+second_number = float(input("Enter the second number: "))
+
 # print the smaller number.
+print(min(first_number, second_number))
