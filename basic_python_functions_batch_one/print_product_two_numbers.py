6756efa1b221d6da33e7fc2fb6a6fe55652c6241
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 17:23:13 +0800

    get the second number.

--- basic_python_functions_batch_one/print_product_two_numbers.py
+++ basic_python_functions_batch_one/print_product_two_numbers.py
@@ -2,4 +2,6 @@
 first_number = int(input("Enter the first number: "))
 
 # ask for the second number.
+second_number = int(input("Enter the second number: "))
+
 # print the product of the two numbers.
