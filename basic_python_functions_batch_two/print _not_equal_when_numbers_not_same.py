5fb1aa495c8294bedb16ee6055c653d3f75ef436
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 22:28:46 +0800

    print "Not Equal" if the two numbers are not the same.

--- basic_python_functions_batch_two/print _not_equal_when_numbers_not_same.py
+++ basic_python_functions_batch_two/print _not_equal_when_numbers_not_same.py
@@ -1,5 +1,8 @@
 # ask for the first number.
-input("Enter the first number: ")
+first_number = float(input("Enter the first number: "))
 # ask for the second number.
-input("Enter the second number: ")
+second_number = float(input("Enter the second number: "))
+
 # print "Not Equal" if the two numbers are not the same.
+if first_number != second_number:
+    print("Not Equal")
