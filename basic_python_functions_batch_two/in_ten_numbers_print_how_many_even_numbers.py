1d2393deeeb8707cd02da1104f4c1e1888a24f08
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 23:33:25 +0800

    set count to 0 and set sum to 0.

--- basic_python_functions_batch_two/in_ten_numbers_print_how_many_even_numbers.py
+++ basic_python_functions_batch_two/in_ten_numbers_print_how_many_even_numbers.py
@@ -1,4 +1,6 @@
 # set count to 0 and set sum to 0.
+code = 0
+sum = 0
 # make a while loop that will run ten times to ask for a number.
     # make a condition to check if the number is even and list it.
 # print the counted even numbers.
