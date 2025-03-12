e737dcc5fbe6e38def8e43735ca1aa4f0fff9c1c
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-12 17:52:21 +0800

    ask for the second number.

--- basic_python_functions_batch_one/print_result_when_first_number_raised_to_second_number.py
+++ basic_python_functions_batch_one/print_result_when_first_number_raised_to_second_number.py
@@ -2,4 +2,6 @@
 input("Enter the first number: ")
 
 # ask for the second number.
+input("Enter the second number: ")
+
 # print the result of raising the first number to the power of the second number.
