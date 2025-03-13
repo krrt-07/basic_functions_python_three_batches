072c262b88db827c145301f20e9e6f3913e565c2
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 02:41:46 +0800

    Initialize an empty list to store numbers.

--- basic_python_functions_batch_four/input_numbers_until_invalid_display_average.py
+++ basic_python_functions_batch_four/input_numbers_until_invalid_display_average.py
@@ -1,4 +1,5 @@
 # Initialize an empty list to store numbers.
+numbers = []
 # Use while loop to continue ask the user while the condition is true.
 # Check if the input is not a valid integer.
 # Convert input to integer.
