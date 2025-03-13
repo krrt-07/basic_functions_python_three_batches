218672d2d5696953fd9bc16e537aec9265debde2
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 21:22:20 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_three/input_numbers_until_invalid_display_unique_or_duplicate.py
@@ -0,0 +1,6 @@
+# Initialize an empty list to store numbers.
+# Use while loop to continue ask the user until they break the condition.
+# Make condition that check if the input is not a valid integer.
+# Convert input to integer.
+# Check if the number is already in the list.
+# print the number.
