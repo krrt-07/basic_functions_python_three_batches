c7d06fb1065635d4b1501441476128014aac4c13
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 16:32:40 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_two/print_all_numbers_between_two_numbers.py
@@ -0,0 +1,4 @@
+# ask for the starting number.
+# ask for the ending number.
+# use for-loop to run multiple times according to user.
+# print the number between the starting number and ending number.
