a9d8d92bc32ded1dc1fe2a46000b33c47a322c91
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 21:10:45 +0800

    make condition to get the only the first entry.

--- basic_python_functions_batch_three/input_10_numbers_display_all_numbers_for_numbers_with_duplicate_display_only_the_first_entry.py
+++ basic_python_functions_batch_three/input_10_numbers_display_all_numbers_for_numbers_with_duplicate_display_only_the_first_entry.py
@@ -4,4 +4,6 @@ numbers = []
 for i in range(10):
     num = int(input(f"Number {i+1}: "))
 # make condition to get the only the first entry.
+    if num not in numbers:
+        numbers.append(num)
 # Display the numbers (only first occurrences of duplicates).
