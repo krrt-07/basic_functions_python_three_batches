03e2e6be7e7a59416cd8c5037568056efad09161
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 02:41:25 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_four/input_numbers_until_invalid_display_average.py
@@ -0,0 +1,6 @@
+# Initialize an empty list to store numbers.
+# Use while loop to continue ask the user while the condition is true.
+# Check if the input is not a valid integer.
+# Convert input to integer.
+# Add the valid number to the list.
+# Calculate and display the average.
