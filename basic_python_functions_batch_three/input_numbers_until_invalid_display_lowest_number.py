6e6338db018d42e02357ff8f524acfc69c02b4ba
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 21:53:56 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_three/input_numbers_until_invalid_display_lowest_number.py
@@ -0,0 +1,5 @@
+# Initialize an empty list to store numbers.
+# Use while loop to continue ask the user until they break the condition.
+# Make condition that check if the input is not a valid integer.
+# Convert input to integer and Add the valid number to the list.
+# Find and display the lowest number.
