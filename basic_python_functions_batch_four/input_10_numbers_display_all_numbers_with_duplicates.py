6740f2a369558e6d55621fc4583ee683f2c5b398
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 01:27:30 +0800

    Use for-loop and ask the user to input 10 numbers.

--- basic_python_functions_batch_four/input_10_numbers_display_all_numbers_with_duplicates.py
+++ basic_python_functions_batch_four/input_10_numbers_display_all_numbers_with_duplicates.py
@@ -1,5 +1,8 @@
 # Initialize an empty list to store numbers.
 numbers = []
 # Use for-loop and ask the user to input 10 numbers.
+for i in range(10):
+    number = int(input(f"Number {i+1}: "))
+    numbers.append(number)
 # Find numbers that have duplicates by using another for-loop.
 # Display only the numbers with duplicates
