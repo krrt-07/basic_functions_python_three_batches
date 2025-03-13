0bbfe5f2df75c8c25f5fa3b256b523d6975ed744
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 16:14:38 +0800

    make condition to disregard numbers ends with 0 or 5.

--- basic_python_functions_batch_two/print_all_numbers_starting_from_zero_to_onehundred_except_numbers_ending_zero_or_ending_five.py
+++ basic_python_functions_batch_two/print_all_numbers_starting_from_zero_to_onehundred_except_numbers_ending_zero_or_ending_five.py
@@ -5,5 +5,7 @@ sum = 0
 # use while loop to run 100 times.
 while count < 100:
     count += 1
+    sum += 1
     # make condition to disregard numbers ends with 0 or 5.
+    if sum % 10 != 0 or sum % 5 != 0:
         # print the number.
