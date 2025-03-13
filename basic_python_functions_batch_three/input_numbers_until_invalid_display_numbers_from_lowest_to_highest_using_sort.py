70d9a18c41693f8f3395cd4bc6ff46590db60567
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 22:36:20 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_three/input_numbers_until_invalid_display_numbers_from_lowest_to_highest_using_sort.py
@@ -0,0 +1,7 @@
+# Initialize an empty list to store numbers.
+# Use while loop to continue ask the user until they break the condition.
+# Check if the input is not a valid integer.
+# Convert input to integer.
+# Add the valid number to the list.
+# Sort the list from lowest to highest.
+# Display the sorted numbers.
