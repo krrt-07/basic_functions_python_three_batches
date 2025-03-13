3f1f70890188069a6e94762f90c0f7d58da0e718
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 01:53:45 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_four/input_numbers_until_invalid_display_highest_number.py
@@ -0,0 +1,6 @@
+# Initialize an empty list to store numbers.
+# Use while loop to continue ask the user while the condition is true.
+# Check if the input is not a valid integer.
+# Convert input to integer.
+# Add the valid number to the list.
+# find the Highest number of all of the numbers by making a condition and using maximum "max()" and print it.
