93114c30670b3ec16f9cc88016b30ddb2a4e843d
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 01:57:20 +0800

    find the Highest number of all of the numbers by making a condition and using maximum "max()" and print it.

--- basic_python_functions_batch_four/input_numbers_until_invalid_display_highest_number.py
+++ basic_python_functions_batch_four/input_numbers_until_invalid_display_highest_number.py
@@ -12,3 +12,6 @@ while True:
 # Add the valid number to the list.
     numbers.append(num)
 # find the Highest number of all of the numbers by making a condition and using maximum "max()" and print it.
+if numbers:
+    highest_number = max(numbers)
+    print(f"The highest number entered is: {highest_number}")
