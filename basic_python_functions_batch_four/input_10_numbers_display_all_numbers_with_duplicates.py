b364db931a55b93219e640b86ccb4313335733f5
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 01:21:23 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_four/input_10_numbers_display_all_numbers_with_duplicates.py
@@ -0,0 +1,4 @@
+# Initialize an empty list to store numbers.
+# Use for-loop and ask the user to input 10 numbers.
+# Find numbers that have duplicates by using another for-loop.
+# Display only the numbers with duplicates
