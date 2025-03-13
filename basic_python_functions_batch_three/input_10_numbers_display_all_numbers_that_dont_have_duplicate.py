07af87b431e585ee60a2b123195841e13f53f6b8
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 21:01:23 +0800

    Find numbers that appear only once.

--- basic_python_functions_batch_three/input_10_numbers_display_all_numbers_that_dont_have_duplicate.py
+++ basic_python_functions_batch_three/input_10_numbers_display_all_numbers_that_dont_have_duplicate.py
@@ -3,5 +3,7 @@ numbers = []
 for i in range(10):
     num = int(input(f"Number {i+1}: "))
     numbers.append(num)
+
 # Find numbers that appear only once.
+unique_numbers = [num for num in numbers if numbers.count(num) == 1]
 # Display the unique numbers
