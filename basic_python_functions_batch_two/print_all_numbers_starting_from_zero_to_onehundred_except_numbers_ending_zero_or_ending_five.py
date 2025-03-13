4bba894ad5830d0a172d8a90da66dd52e7ee05fd
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 16:12:55 +0800

    use while loop to run 100 times.

--- basic_python_functions_batch_two/print_all_numbers_starting_from_zero_to_onehundred_except_numbers_ending_zero_or_ending_five.py
+++ basic_python_functions_batch_two/print_all_numbers_starting_from_zero_to_onehundred_except_numbers_ending_zero_or_ending_five.py
@@ -1,6 +1,9 @@
 # set count and sum equal to 0.
 count = 0
 sum = 0
+
 # use while loop to run 100 times.
+while count < 100:
+    count += 1
     # make condition to disregard numbers ends with 0 or 5.
         # print the number.
