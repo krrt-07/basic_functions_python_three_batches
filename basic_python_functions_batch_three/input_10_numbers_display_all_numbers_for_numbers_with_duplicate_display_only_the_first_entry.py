278bb7c537ef0f5ac1c4bc02f89b54fbc2f0660a
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 21:09:28 +0800

    set numbers to empty set.

--- basic_python_functions_batch_three/input_10_numbers_display_all_numbers_for_numbers_with_duplicate_display_only_the_first_entry.py
+++ basic_python_functions_batch_three/input_10_numbers_display_all_numbers_for_numbers_with_duplicate_display_only_the_first_entry.py
@@ -1,4 +1,5 @@
 # set numbers to empty set.
+numbers = []
 # Ask the user to input 10 numbers.
 # make condition to get the only the first entry.
 # Display the numbers (only first occurrences of duplicates).
