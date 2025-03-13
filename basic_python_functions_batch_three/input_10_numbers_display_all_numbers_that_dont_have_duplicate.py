0d8c204270dcd5b5f73666ac71b6522a9b4925f7
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 21:00:49 +0800

    Ask the user to input 10 numbers.

--- basic_python_functions_batch_three/input_10_numbers_display_all_numbers_that_dont_have_duplicate.py
+++ basic_python_functions_batch_three/input_10_numbers_display_all_numbers_that_dont_have_duplicate.py
@@ -1,3 +1,7 @@
 # Ask the user to input 10 numbers.
+numbers = []
+for i in range(10):
+    num = int(input(f"Number {i+1}: "))
+    numbers.append(num)
 # Find numbers that appear only once.
 # Display the unique numbers
