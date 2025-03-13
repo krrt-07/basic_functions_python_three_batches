5f3d6fe106addd8bac5dc936659afe44a8644567
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 01:41:08 +0800

    # Initialize an empty list to store numbers.

--- basic_python_functions_batch_four/input_numbers_until_invalid_display_number_with_most_duplicates.py
+++ basic_python_functions_batch_four/input_numbers_until_invalid_display_number_with_most_duplicates.py
@@ -1,4 +1,5 @@
 # Initialize an empty list to store numbers.
+numbers = []
 # Use while loop to continue ask the user while the condition is true.
 # Make a condition that will check if the input is not a valid integer.
 # Convert input to integer.
