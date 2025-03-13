b37d4b569c5c624cb138984a23abf9b0e4ce81ff
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 21:23:42 +0800

    Make condition that check if the input is not a valid integer.

--- basic_python_functions_batch_three/input_numbers_until_invalid_display_unique_or_duplicate.py
+++ basic_python_functions_batch_three/input_numbers_until_invalid_display_unique_or_duplicate.py
@@ -4,6 +4,9 @@ numbers = []
 while True:
     user_input = input("Enter a number: ")
 # Make condition that check if the input is not a valid integer.
+    if not user_input.isdigit():
+        print("Invalid input. Stopping the program.")
+        break
 # Convert input to integer.
 # Check if the number is already in the list.
 # print the number.
