aef4e648927304c4269f9e5aea9bf82044c64d9c
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 16:33:36 +0800

    ask for the starting number.

--- basic_python_functions_batch_two/print_all_numbers_between_two_numbers.py
+++ basic_python_functions_batch_two/print_all_numbers_between_two_numbers.py
@@ -1,4 +1,5 @@
 # ask for the starting number.
+start_number = int(input("Enter the starting number: "))
 # ask for the ending number.
 # use for-loop to run multiple times according to user.
 # print the number between the starting number and ending number.
