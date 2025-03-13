9b68c727de7d8336797089ff42bbd57696a9e624
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 01:28:02 +0800

    Find numbers that have duplicates by using another for-loop.

--- basic_python_functions_batch_four/input_10_numbers_display_all_numbers_with_duplicates.py
+++ basic_python_functions_batch_four/input_10_numbers_display_all_numbers_with_duplicates.py
@@ -5,4 +5,8 @@ for i in range(10):
     number = int(input(f"Number {i+1}: "))
     numbers.append(number)
 # Find numbers that have duplicates by using another for-loop.
+duplicates = []
+for num in numbers:
+    if numbers.count(num) > 1 and num not in duplicates:
+        duplicates.append(num)
 # Display only the numbers with duplicates
