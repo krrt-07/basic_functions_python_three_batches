af06e14eb01702fe08481e0406a2ec76ebc82193
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-13 21:54:47 +0800

    Use while loop to continue ask the user until they break the condition.

--- basic_python_functions_batch_three/input_numbers_until_invalid_display_lowest_number.py
+++ basic_python_functions_batch_three/input_numbers_until_invalid_display_lowest_number.py
@@ -1,6 +1,8 @@
 # Initialize an empty list to store numbers.
 numbers = []
 # Use while loop to continue ask the user until they break the condition.
+while True:
+    user_input = input("Enter a number: ")
 # Make condition that check if the input is not a valid integer.
 # Convert input to integer and Add the valid number to the list.
 # Find and display the lowest number.
