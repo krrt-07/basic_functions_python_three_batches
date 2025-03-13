952294cdc403a20056cfb19421cfaa16e4d625fd
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 01:38:14 +0800

    pseudocode made.

---
+++ basic_python_functions_batch_four/input_numbers_until_invalid_display_number_with_most_duplicates.py
@@ -0,0 +1,6 @@
+# Initialize an empty list to store numbers.
+# Use while loop to continue ask the user while the condition is true.
+# Make a condition that will check if the input is not a valid integer.
+# Convert input to integer.
+# Add the valid number to the list.
+# Find the number with the most duplicates by making a condition using maximum "max()" and print it.
