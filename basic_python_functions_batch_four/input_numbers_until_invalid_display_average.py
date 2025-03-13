014f94c83b09d69380c3f1d4b8a104e9d294af17
krrt-07 <jacinkurt.ocampo06@gmail.com>
2025-03-14 02:43:08 +0800

    Convert input to integer.

--- basic_python_functions_batch_four/input_numbers_until_invalid_display_average.py
+++ basic_python_functions_batch_four/input_numbers_until_invalid_display_average.py
@@ -8,5 +8,6 @@ while True:
         print("Invalid input. Stopping the program.")
         break
 # Convert input to integer.
+num = int(user_input)
 # Add the valid number to the list.
 # Calculate and display the average.
